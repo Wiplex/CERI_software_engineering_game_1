528bd5e9d01457fceb191d1b86e0a887b17e7d43
Thibaulltt <devillelethibault@gmail.com>
2017-04-14 22:37:34 +0200

    added constructor, destructor and beginning function

--- headers/fonctionsjeu.h
+++ headers/fonctionsjeu.h
@@ -1,9 +1,17 @@
-// #include "../headers/carte.h"
-// #include "../headers/perso.h"
+// #include "./carte.h"
+// #include "./perso.h"
+// #include "./io.h"
+
+// Normalement, à ca point là, la carte doit être chargée, le personnage est choisi et chargé, les monstres sont chargés et il ne reste qu'à lancer le jeu.
 
 class jeu
 {
 	// carte jeu_carte;
 	// perso jeu_perso;
 	int jeu_nombre_monstre;
+
+public:
+	jeu();
+	~jeu();
+	void demarrer_jeu();
 }
--- sources/fonctionsjeu.cpp
+++ sources/fonctionsjeu.cpp
@@ -2,3 +2,19 @@
 #include <iostream>	// à remplacer avec notre librairie I/O plus tard
 
 using namespace std;
+
+jeu::jeu()
+{
+	// jeu_perso = new perso();			// À COMPLÉTER UNE FOIS QUE CLASSE PERSO FINIE
+	// jeu_carte = new carte();			// À COMPLÉTER UNE FOIS QUE CLASSE CARTE FINIE
+	// jeu_nombre_monstre = carte.getMonsters();	// À COMPLÉTER UNE FOIS QUE CLASSE CARTE FINIE
+}
+
+jeu::~jeu()
+{}
+
+void jeu::demarrer_jeu()
+{
+	cout << "Bienvenue dans le jeu" << endl;
+	// custom_buffer.affichercarte(jeu_carte);
+}
