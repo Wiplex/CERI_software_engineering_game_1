bf8bbb4d13e383583b861a9436db07e31cc28940
Thibaulltt <devillelethibault@gmail.com>
2017-04-14 22:06:40 +0200

    added game logic
    ajouté la logique de jeu

---
+++ headers/fonctionsjeu.h
@@ -0,0 +1,9 @@
+// #include "../headers/carte.h"
+// #include "../headers/perso.h"
+
+class jeu
+{
+	// carte jeu_carte;
+	// perso jeu_perso;
+	int jeu_nombre_monstre;
+}
---
+++ sources/fonctionsjeu.cpp
@@ -0,0 +1,4 @@
+#include "../headers/fonctionsjeu.h"
+#include <iostream>	// à remplacer avec notre librairie I/O plus tard
+
+using namespace std;
