bccf6fd389a831063e2b4c4a79c57afb6c9225b8
Thibaulltt <devillelethibault@gmail.com>
2017-04-16 16:17:27 +0200

    changed buffer type
    changes :
    
    - buffer from : std::streambuf to std::basic_istream<char,
    char_traits<char>>
    - no longer a class
    - now a namespace

--- headers/io.h
+++ headers/io.h
@@ -1,10 +1,12 @@
 //! cette classe sera une classe permettant de définir un buffer custom pour les input, ainsi que de pouvoir afficher tout ce que l'on souhaite.
 using namespace std;
 
-class io : public streambuf
+namespace io
 {
-	std::streambuf* buf_;
-	char* input_;
+	std::basic_istream<char, char_traits<char>> io_input;
 	public:
 		io();
+		~io();
+		char de();
+		void vider();
 }
