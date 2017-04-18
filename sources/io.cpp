c900280e1ef2f7d20179518044bf9d61834b8ce9
Thibaulltt <devillelethibault@gmail.com>
2017-04-18 02:47:37 +0200

    cleaned up, commented
    cleaned up (a little bit) the code, added a note at the start for later
    work, and generally redid a bit the commenting
    
    not yet ready for doxygen.

--- sources/io.cpp
+++ sources/io.cpp
@@ -2,39 +2,33 @@
 #include <termios.h>	// Needed for terminal input manipulation
 #include <stdio.h>	// Needed for terminal input manipulation
 
+// Notes :
+// Get the terminal column number :
+// 	> stty -a | grep "col" | tail -c 22 | tail -c 13 | head -c 3
+// Get the terminal rows number :
+// 	> stty -a | grep "col" | tail -c 22 | head -c 2
+
 namespace io {
 	char de()
 	{
-		// Create new structures for
-		// storing new and old configs
-		// of the current terminal.
-		static struct termios old;
-		static struct termios mew;
-		// Return character
-		char ch;
-
-		/* Initialize new terminal i/o settings */
-		// initTermios(int echo)
-		tcgetattr(0, &old);			/* grab old terminal i/o settings */
-		mew = old;				/* make new settings same as old settings */
-		mew.c_lflag &= ~ICANON;			/* disable buffered i/o */
-		mew.c_lflag &= ~ECHO;			/* set echo mode */
-		tcsetattr(0, TCSANOW, &mew);		/* use these new terminal i/o settings now */
-		// ~initTermios(int echo)
-
-		ch = getchar();
-
-		/* Restore old terminal i/o settings */
-		// resetTermios()
+		// Create new structures for storing new and old configs of the current terminal.
+		static struct termios old, mew;
+		char ch;				// Return character
+		// Initialize new terminal I/O settings
+		tcgetattr(0, &old);			/* Grab old terminal i/o settings */
+		mew = old;				/* Make new settings same as old settings */
+		mew.c_lflag &= ~ICANON;			/* Disable buffered i/o */
+		mew.c_lflag &= ~ECHO;			/* Set echo mode */
+		tcsetattr(0, TCSANOW, &mew);		/* Use these new terminal i/o settings now */
+		ch = getchar();				// getchar() now takes only one char
+		// Restore old terminal I/O settings
 		tcsetattr(0, TCSANOW, &old);
-		// ~resetTermios()
-
 		return ch;
 	}
 	void Bienvenue()
 	{
         std::puts("\n");
-        std::puts(  "                                       Welcome to");
+        std::puts("                                         Welcome to");
         std::puts(" __________   ___    ___   __________     __________   __________   __________   __________");
         std::puts("/___   ___/  /  /   /  /  /  _______/    /  _______/  /  ____   /  /   __    /  /  _______/");
         std::puts("   /  /     /  /__ /  /  /  /__         /  /  __     /  /___/  /  /  /   /  /  /  /__");
