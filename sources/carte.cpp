30667c97ecfcc3d4a6177ef068eac4be2c6d7f84
LudivineN <ludivine.nouveau@gmail.com>
2017-04-21 22:32:08 +0200

    carte.cpp carte.h
    les deux fichiers ont été modifiés 
    Aucune fonction n'est restée la même 
    Principales modifications : sauvegarde et chargement

--- sources/carte.cpp
+++ sources/carte.cpp
@@ -1,72 +1,233 @@
-#include "../headers/carte.h"
+#include "carte.h"
 #include <iostream>
 #include <fstream>
 #include <limits>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 Carte :: Carte()
 {
-
+	this->nom = "";
+	this->description = "";
+	this->plateau = new string * ;
+	this -> case_dispo = 0;
 }
 
 Carte :: Carte (int size, string name, string desc)
 {
+	verif_taille(size);
 	this->nom = name;
 	this->description = desc;
-	this->taille = size ;
-	this->plateau = new char * [taille];
+	this->plateau = new string * [taille];
 	for (int i = 0; i < taille ; i++)
 	{
-		plateau[i] = new char [taille];
+		plateau[i] = new string [taille];
 		for (int j = 0 ; j< taille ; j++)
 		{
-			plateau[i][j] = /*remplir client*/'?' ;
+			plateau[i][j] = "v" ;
+			
 		}
 	}
+	this -> case_dispo= taille*taille;
 }
+
+
+void Carte::verif_taille(int size)
+{
+	while ( size < 4  || size > 255)
+	{
+		cout << "Taille impossible veuillez choisir une taille entre 5 et 255 "<<endl << "choix de la taille :" << endl;
+		cin >> size;
+	}
+	this-> taille=size;
+}
+
+void Carte :: coordonneejoueur()
+{
+	int coordonneejoueur1;
+	int coordonneejoueur2;
+	bool placement_fait = false;
+	while (placement_fait==false)
+	{
+		cout << "Choisissez la premiere coordonnée où le joueur doit apparaitre :" ;
+		cin >> coordonneejoueur1;
+		cout << "Choisissez la deuxieme coordonnée où le joueur doit apparaitre :" ;
+		cin >> coordonneejoueur2;
+		if (coordonneejoueur1 < taille && coordonneejoueur2 < taille)
+		{
+			plateau[coordonneejoueur1][coordonneejoueur2]="j";	
+			placement_fait=true;
+		}
+		else 
+		{
+			cout << "vos coordonnées sont hors de la carte!!" << endl;
+		}
+	}
+	case_dispo = case_dispo - 1;
+}	
+
+void Carte::coordonneeobstacle()
+{
+	int nbr_obstacle=0;
+	int coordonneeobstacle1;
+	int coordonneeobstacle2;
+	cout<< "Combien d'obstacle, sur la carte, voulez vous?";
+	cin >> nbr_obstacle;
+	while (nbr_obstacle >= case_dispo)
+	{
+		cout<< "Vous avez choisi trop d'obstacle." << endl << "Combien d'obstacle voulez vous sur la carte?";
+		cin >> nbr_obstacle;
+	}
+	int i = 1;
+	while (i <= nbr_obstacle)
+	{
+		cout << "Choisissez la premiere coordonnée où l'obstacle doit apparaitre :" ;
+		cin >> coordonneeobstacle1;
+		cout << "Choisissez la deuxieme coordonnée où l'obstacle doit apparaitre :" ;
+		cin >> coordonneeobstacle2;
+		if (coordonneeobstacle1 < taille && coordonneeobstacle2 < taille)
+		{
+			if (plateau[coordonneeobstacle1][coordonneeobstacle2] == "v")
+			{
+				plateau[coordonneeobstacle1][coordonneeobstacle2] = "o";
+				i++;
+			}
+			else 
+			{
+				cout << "cette case est deja occupée, Veuillez en choisir une autre " << endl; 
+			}
+		}
+		else
+		{
+			cout << "Vos coordonnées sont hors de la carte!!" << endl;
+		}	
+	}
+	case_dispo = case_dispo - nbr_obstacle; 
+}
+
+void Carte::coordonneemonstre()
+{
+	int nombre_monstre = 0;
+	int coordonneemonstre1;
+	int coordonneemonstre2;
+	while ((nombre_monstre < taille) || (nombre_monstre > case_dispo))
+	{
+		cout<< "Combien de monstre voulez-vous sur la carte?";
+		cin >> nombre_monstre;
+	}
+	this -> nbr_monstre = nombre_monstre;
+	int i = 1;
+	while (i <= nombre_monstre)
+	{
+		cout << "Choisissez la premiere coordonnée où le monstre doit apparaitre :" ;
+		cin >> coordonneemonstre1;
+		cout << "Choisissez la deuxieme coordonnée où le monstre doit apparaitre :" ;
+		cin >> coordonneemonstre2;
+		if (coordonneemonstre1 < taille && coordonneemonstre2 < taille)
+		{
+			if (plateau[coordonneemonstre1][coordonneemonstre2] == "v")
+			{
+				plateau[coordonneemonstre1][coordonneemonstre2] = "m";
+				i++;
+			}
+			else 
+			{
+				cout << "Cette case est deja occupée veuillez en choisir une autre " << endl; 
+			}
+		}
+		else
+		{ 
+			cout << "Vos coordonnées sont hors de la carte!!" << endl;
+		}
+	}
+	case_dispo = case_dispo - nbr_monstre;
+}
+
+void Carte :: affichage_normal()
+{
+	for (int i=0; i<taille; i++)
+	{
+		for (int j=0; j<taille; j++)
+		{
+			cout << plateau[i][j];
+		}
+		cout << endl;
+	}
+}
+
+int Carte::nbLigneFichier(string nomFichier) //Compte le nb de ligne du fichier pour créer l'identifiant unique d'un monstre
+{
+	ifstream fichier(nomFichier.c_str()); //Ouverture en mode lecture
+	int nbLigne = 0;
+	if(fichier)
+	{
+		string ligne="";
+
+		while(getline(fichier, ligne)) //Stockage de chaque ligne du fichier
+		{
+			nbLigne++;
+		}
+		fichier.close(); //On ferme le fichier
+	}
+
+	else
+	{
+		cerr << "Impossible d'ouvrir le fichier." << endl;
+	}
+
+	return nbLigne;
+}
+
 
 void Carte :: sauvegarde()
 {
 	// Nom de la carte
-	string nom_de_la_carte ;
-	string carte ;
-	cout << "Choisir le nom de la carte : " << endl ;
-	cin >> nom_de_la_carte ;
-	carte = nom_de_la_carte + ".txt" ;
-
-	// Vérification que le fichier n'existe pas déjà
-	if (!carte_existe(carte))
+	string carte = "fichierCarte.txt" ;
+	string type_obstacle;
+	string monstre;
+	int nbligne=nbLigneFichier(carte);
+	
+	//ouverture du fichier en écriture
+	ofstream fichier(carte, ios :: app) ;
+		
+	// Si fichier bien ouvert
+	if (fichier)
 	{
-		//ouverture du fichier en écriture
-		ofstream fichier(carte, ios :: out | ios :: trunc) ;
-
-		// Si fichier bien créé
-		if (fichier)
+		fichier << "c" << nbligne << "|" ;
+		fichier << nom << "|" ;
+		fichier << description << "|";
+		fichier << taille << "|";
+		for (int i = 0 ; i < taille; i++)
 		{
-			for (int i = 0 ; i < taille; i++)
+			for (int j = 0 ; j < taille ; j++)
 			{
-				for (int j = 0 ; j < taille ; j++)
+				if (plateau[i][j]=="o")
 				{
-					fichier << plateau[i][j] ;
+					cout << "Quelle est l'obstacle à la case "<< i << " "<< j << ":";
+					cin >> type_obstacle;
+					fichier << "(" << i << "," << j << ","<< type_obstacle << ")"; 
 				}
-				fichier << '\n' ;
-			}
-			// On referme le fichier
-			fichier.close() ;
-			cout << "Carte sauvegardée" << endl ;
+				if (plateau[i][j]=="m")
+				{
+					cout << "Quelle est le monstre à la case "<< i << " "<< j << ":";
+					cin >> monstre;
+					fichier << "(" << i << "," << j << ","<< monstre << ")"; 
+				}
+				if (plateau[i][j]=="j")
+					fichier << "(" << i << "," << j << ",joueur" << ")"; 
+			} 
+			
 		}
-		else cerr << "Echec de la sauvegarde" << endl ;
-		return ;
-	}
-	else
-	{
-		cout << "Nom existant" << endl ;
-		sauvegarde() ;
-	}
-
+		fichier << '\n' ;
+		// On referme le fichier
+		fichier.close() ;
+		cout << "Carte sauvegardée" << endl ;
+	} 
+	else cerr << "échec de la sauvegarde" << endl ;
+	return ;
 }
-
 
 string Carte :: getName()
 {
@@ -78,61 +239,107 @@ string Carte :: getDescription()
     return this->description;
 }
 
-bool Carte :: carte_existe(string nom)
+
+
+
+vector <Carte> Carte :: chargement ()
 {
-	ifstream fichier(nom , ios :: in) ;
+	vector <Carte> selectionnable ;
+	ifstream fichier("fichierCarte.txt", ios :: in) ;
 	if (fichier)
 	{
-		fichier.close() ;
-		return true ;
-	}
-	else
-	{
-		fichier.close() ;
-		return false ;
-	}
-}
-
-
-void Carte :: chargement (string nom_selection)
-{
-	string carte = nom_selection + ".txt" ;
-	if (carte_existe(carte))
-	{
-	    cout << "existe";
-		ifstream fichier(carte, ios:: in) ;
-		if (fichier)
+		string current_line;
+		Carte carte_temporaire ;
+		while (getline(fichier, current_line))
 		{
-			this->taille = quel_taille(carte) ;
-			for (int i = 0 ; i < taille ; i++ )
+			string id = "" ;
+			string nom = "" ;
+			string description = "" ;
+			string taille= "";
+			string coordonnee1 = "" ; 
+			string coordonnee2 = "" ;
+			string type = "" ;
+			int count = 0 ;
+			int count_coordonnee = 0 ;
+			int i = 0 ;
+
+			while (current_line[i+1] != '\0')
 			{
-				for (int j = 0 ; j < taille ; j++)
+				char tmp = current_line[i] ;
+				if (tmp == '|')
 				{
-					fichier >> plateau[i][j];
+					count ++ ;
+					i++ ;
+				}
+				else if (count == 0)
+				{
+					id = id + tmp ;
+					i++ ;
+				}
+				else if (count == 1)
+				{
+					nom = nom + tmp ;
+					i++ ;
+				}
+				else if (count == 2)
+				{
+					description = description + tmp ;
+					i++ ;
+				}
+				else if (count == 3)
+				{
+					taille = taille + tmp ;
+					i++ ;
+				}
+				int t = atoi(taille.c_str());
+				carte_temporaire.taille = t ;
+				carte_temporaire.nom = nom ;
+				carte_temporaire.description = description ;
+				if (count == 4)
+				{
+					if (tmp == ')')
+					{
+						count_coordonnee = 0 ;
+						i++ ;
+						int coor1 = atoi(coordonnee1.c_str()) ;
+						int coor2 = atoi(coordonnee1.c_str()) ;
+						carte_temporaire.plateau[coor1][coor2]= type;
+					}
+					else if (tmp == ',')
+					{
+						count_coordonnee ++ ;
+						i++ ;
+					}
+					else if (tmp == '(')
+					{
+						i ++ ;
+					}
+					else if ((tmp != '(') && (tmp != ')') && (tmp != ','))
+					{
+						if (count_coordonnee == 0)
+						{
+							coordonnee1 = coordonnee1 + tmp ;
+							i ++ ;
+						}
+						else if (count_coordonnee == 1)
+						{
+							coordonnee2 = coordonnee2 + tmp ;
+							i++ ;
+						}
+						else if (count_coordonnee == 2)
+						{
+							type = type + tmp ;
+							i++ ;
+						}
+					}
 				}
 			}
 		}
+		selectionnable.push_back(carte_temporaire) ;
 	}
+	return selectionnable ;
 }
 
-int Carte :: quel_taille(string nom)
-{
-	ifstream fichier(nom, ios :: in) ;
-	int count = -1 ;
-	char current ;
-	while (current != '\n')
-	{
-		fichier.get(current) ;
-		count ++ ;
-	}
-	fichier.close() ;
-	return count ;
-}
 
-void Carte :: suppression(string nom)
-{
-	string nom_carte = nom + ".txt" ;
-	//delete nom_carte ;
-	if( carte_existe(nom_carte)) cout << "Echec de la suppression" << endl ;
-	else cout << "Carte supprimée" << endl ;
-}
+
+
