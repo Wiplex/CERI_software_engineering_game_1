6ee2e867885d97adaabaab8c63d3809ab5bc4f5d
Thibaulltt <devillelethibault@gmail.com>
2017-04-22 22:34:16 +0200

    dammit, Replace function. You are too strong

--- headers/monstre.h
+++ headers/monstre.h
@@ -8,24 +8,24 @@ class monstre : public entite
 {
 
 public:
-	//! Constructeur ventiteIde
+	//! Constructeur vide
 	/*
-		Crée un monstre ventiteIde.
-		\warning Le monstre sera ventiteIde. Cela signifie qu'il ne sera pas utilisable pour le jeu, sa vie étant égale à 0
+		Crée un monstre vide.
+		\warning Le monstre sera vide. Cela signifie qu'il ne sera pas utilisable pour le jeu, sa vie étant égale à 0
 		\post Le monstre crée aura les paramètres suivants:
 		- entiteName = "Inconnu"
 		- entiteHpMax = 0
 		- entiteHpCurrent = 0
 		- entiteSpeed = 0
 		- entiteAlive = true (sera changé immédiatement en false)
-		- entiteSkillVect = <vecteur ventiteIde>
+		- entiteSkillVect = <vecteur vide>
 	*/
 	monstre():entite(){};
 
 
 	//! Constructeur avec tout
 	/*!
-		\param entiteId L'entiteIdentifiant du monstre
+		\param entiteId L'identifiant du monstre
 		\param entiteName Le nom du monstre
 		\param entiteHpMax Les points de vie max du monstre
 		\param entiteSpeed La vitesse du monstre
