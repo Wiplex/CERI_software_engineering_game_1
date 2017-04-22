5a2702afb8ac052d08750129ff0da6250626a9ff
Loic MENGUY <loicm97@gmail.com>
2017-04-22 19:16:16 +0200

    Add files via upload
    ajout des entites (superclasse pour perso et monstre)
    modification de la fonction enleverMana
    trucs chiants

--- headers/monstre.h
+++ headers/monstre.h
@@ -1,23 +1,11 @@
-#include <string>
-#include <vector>
-#include "competence.h"
+#include "../headers/entite.h"
 
 #ifndef MONSTRE_H
 #define MONSTRE_H
 #pragma once
 
-class monstre
+class monstre : public entite
 {
-protected:
-	std::string id;
-	std::string name;
-	std::string description;
-	int hpMax;
-	int hpCurrent;
-	int manaCurrent;
-	int speed;
-	bool alive;
-	std::vector<competence> skillVect;
 
 public:
 	//! Constructeur vide
@@ -32,91 +20,26 @@ public:
 		- alive = true (sera changé immédiatement en false)
 		- skillVect = <vecteur vide>
 	*/
-	monstre();
+	monstre():entite(){};
 
-	//! Constructeur avec carac d'un monstre
-	/*!
-		Constructeur assignant tout, sauf les compétences, qui ne seront pas rajoutées (pas de fonctions).
-		\param name Le nom du monstre.
-		\param hpMax Le nombre de points de vie max du monstre.
-		\param speed La vitesse d'attaque du monstre.
-	*/
-	monstre(std::string name, int hpMax, int speed);
 
-	//! Constructeur avec les compétences
+	//! Constructeur avec tout
 	/*!
-		\param name Le nom du monstre.
-		\param hpMax Le nombre de points de vie max du monstre.
-		\param speed La vitesse d'attaque du monstre.
+		\param id L'identifiant du monstre
+		\param name Le nom du monstre
+		\param hpMax Les points de vie max du monstre
+		\param speed La vitesse du monstre
+		\param manaMax Les points de mana max du monstre
+		\param description La description du monstre
 		\param allSkills Un vecteur (std::vector) contenant toutes les compétences de ce monstre.
 	*/
-	monstre (std::string name, int hpMax, int speed, std::vector<competence> allSkills);
+	monstre(std::string id, std::string name, int hpMax, int speed, int manaMax, std::string description, std::vector<competence> allSkills) : entite(id, name, hpMax, speed, manaMax, description, allSkills ){};
 
-	template<typename T> std::string toString(const T & valeur); // Conversion de n'importe quoi en string
-
-	//!Getter pour l'ID.
-	std::string getId();
-
-	//! Getter pour le nom.
-	std::string getName();
-
-	//! Getter pour la description.
-	std::string getDescription();
-
-	//! Getter pour le nombre de points de vie max.
-	int getHpMax();
-
-	//! Getter pour le nombre de points de vie actuels.
-	int getHpCurrent();
-
-	//! Getter pour la vitesse d'attaque du monstre.
-	int getSpeed();
-
-	//! Getter qui permet de savoir si le monstre est en vie.
-	bool getAlive();
-
-	//! Getter qui renvoie un vecteur (std::vector) de compétences.
-	std::vector<competence> getSkillVect();
-
-	//! Retour d'une string représentant un monstre.
-	/*!
-		Convertit un objet monstre en une ligne de string.
-		\post La string contiendra les infos dans cet ordre :
-		- identifiant (type <code>m\<entier></code>)
-		- nom du monstre
-		- nombre de points de vie
-		- vitesse d'attaque
-		- toutes les compétences , séparées par des <code>:</code>
-	*/
-	std::string monstreString();
-
-	//! Retourne le nombre de lignes d'un fichier.
-	/*!
-		Compte le nb de lignes du fichier pour créer l'identifiant unique d'un monstre. L'identifiant sera <code> nbLignes + 1 </code>
-		\return Un entier représentant le nombre de lignes.
-		\param nomFichier Une string (std::string) qui sera le nom du fichier à ouvrir.
-	*/
-	int nbLigneFichier(std::string nomFichier);
-
-	//! Permet d'écrire le monstre dans un fichier de sauvegarde
-	void saveInFile();
 
 	//! Pour tester
 	void printMonstre();
 
-	//! Enlève x points de vie au monstre.
-	/*!
-		Cette fonction permet d'enlever des points de vie. Elle permet aussi de savoir si un monstre est en vie (ptsVie < 0) ou si il est mort.
-		\return Un booléen qui est égal à <code>true</code> si le monstre est mort, <code>false</code> sinon.
-	*/
-	bool enleverVie(int degats);
 
-	//! Enlève x points de mana au monstre.
-	/*!
-		Cette fonction ne sert à rien, à part ne pas faire bugger les autres.
-		\return Un booléen vérifiant la capacité à dépenser la mana.
-	*/
-	bool enleverMana(int manaCost);
 };
 
 
